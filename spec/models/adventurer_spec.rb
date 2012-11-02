8a70791011f31b2bfb79070c40d6fe3a60ff0bd2
Gabriel Nadler <nadlergabriel@gmail.com>
2012-11-02 21:30:49 +0100

    simplified tests

--- spec/models/adventurer_spec.rb
+++ spec/models/adventurer_spec.rb
@@ -20,22 +20,19 @@
 require 'spec_helper'
 
 describe Adventurer do
-  before :each do
-  @adv = Adventurer.new
-  end
 
   it "should have a first name" do
-    @adv.should respond_to :first_name
+    should respond_to :first_name
   end
   it "should have a name" do
-    @adv.should respond_to :name
+    should respond_to :name
   end
   it "should validate the presence of the name" do
     should validate_presence_of :name
   end
 
   it "should have an email" do
-    @adv.should respond_to :email
+    should respond_to :email
   end
   it "should validate the presence of the email" do
     should validate_presence_of :email
