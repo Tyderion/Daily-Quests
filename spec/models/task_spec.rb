651b4c8c5b6b92336c147f70cec01d864076154d
Gabriel Nadler <nadlergabriel@gmail.com>
2012-11-03 21:04:09 +0100

    Added some more tests for tasks

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe Task do
+  before :each do
+    @task = FactoryGirl.create(:task)
+  end
+
   it "should have a description" do
     should respond_to :description
   end
   it "should respond to title" do
     should respond_to :title
+  end
+
+  it "should respond to private" do
+    should respond_to :private
+  end
+  it "should respond to private?" do
+    expect { @task.private? }.to_not raise_error
+  end
+
+  it "should respond to public?" do
+    expect { @task.public? }.to_not raise_error
   end
   it "should have a type" do
     should respond_to :type
