9644a1009d32cea31b7b2a3d67af9a4b5d1b710d
Gabriel Nadler <nadlergabriel@gmail.com>
2012-11-14 16:41:22 +0100

    Forgot to remove comment

--- app/models/task.rb
+++ app/models/task.rb
@@ -29,10 +29,8 @@ class Task < ActiveRecord::Base
   has_many :subtasks
 
     # Make it not generate an exception when trying to assign an empty type
-    # Don't know why, but TaskType.id_for "Unknown string" returns nil instead of new ID.
-    # Doesn't matter because that's also what it should be like xD
     def type=(t)
-      if t.nil? or t.empty?
+      if t.blank?
         t = "Task"
       end
       id = TaskType.id_for(t) || TaskType.gen_id_for(t)
