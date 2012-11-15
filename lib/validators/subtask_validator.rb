932c67407a8eeff22207c339178ddde64c5ffb89
Gabriel Nadler <nadlergabriel@gmail.com>
2012-11-15 15:53:54 +0100

    SubtaskValidator uses SubtaskCache, empty specfile for subtaskvalidator

--- lib/validators/subtask_validator.rb
+++ lib/validators/subtask_validator.rb
@@ -1,16 +1,30 @@
 class SubtaskValidator
   def initialize(task)
     @task = task
-    @valid_subtasks = []
-    @invalid_subtasks = []
+    @valid= SubtaskCache.new
+    @invalid = SubtaskCache.new
   end
 
   def valid?(task)
-    if task.id == @task.id || !type_valid?(task)
-      return false
+    if @valid.include?(task)
+      true
+    elsif @invalid.include(task)
+      false
+    elsif task.id == @task.id || !type_valid?(task)
+      @invalid.store(task)
+      false
     else
-      return subtasks_valid?(task.subtasks)
+      @valid.store(task)
+      true
     end
+
+    # if !@valid.include?(task) && ( @invalid.include(task) || task.id == @task.id || !type_valid?(task) )
+    #   @invalid.store(task)
+    #   return false
+    # else
+    #   @valid.store(task) unless @valid.include?(task)
+    #   return subtasks_valid?(task.subtasks)
+    # end
   end
 
   def subtasks_valid?(subtasks)
@@ -21,13 +35,15 @@ class SubtaskValidator
     true
   end
 
+
+  #Todo: Nicer type-validation
   def type_valid?(task)
-    if @task.type == TaskType.name_for(1)
-      task.type == TaskType.name_for(1)
-    elsif @task.type == TaskType.name_for(2)
-      task.type == TaskType.name_for(1)
-    elsif @task.type == TaskType.name_for(3)
-      task.type = TaskType.name_for(2)
+    if @task.type == "Task"#TaskType.name_for(1)
+      task.type == "Task"#TaskType.name_for(1)
+    elsif @task.type == "Quest"#TaskType.name_for(2)
+      task.type == "Task"#TaskType.name_for(1)
+    elsif @task.type == "Questsequence"#TaskType.name_for(3)
+      task.type = "Quest"#TaskType.name_for(2)
     end
   end
 end
---
+++ spec/validator/subtask_validator_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe SubtaskValidator do
+  #Todo: Specs for the validator
+end
